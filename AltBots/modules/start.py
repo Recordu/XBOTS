d9990f9a1cec0d68fccafa1032d205b7ba93e690
Adarsh Rajput <117724279+Recordu@users.noreply.github.com>
2023-04-09 17:55:19 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -8,11 +8,11 @@ START_BUTTON = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/TheAltron"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/AltronChats")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/about_lucifer1"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/wilderharley_650")
     ],
     [
-        Button.url("• ʀᴇᴘᴏ •", "https://github.com/ItZxSTaR/XBOTS")
+        Button.url("• ʀᴇᴘᴏ •", "LAND LEGA TO BOL")
     ]
 ]
 
